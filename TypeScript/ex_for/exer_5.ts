d6bf59111a0b086bd107d48124a91460497c8c16
Kauan <kauancph11@gmail.com>
2023-07-24 20:43:09 -0300

    24/07/23

---
+++ TypeScript/ex_for/exer_5.ts
@@ -0,0 +1,20 @@
+import PromptSync from 'prompt-sync';
+let prompt = PromptSync();
+
+/* 5. Imprimir os primeiros 10 números da sequência de Fibonacci. */
+export function fibonacci() {
+   let num1: number = 0;
+   let num2: number = 1;
+   let auxiliar: number;
+
+   for (var i = 0; i < 10; i++) {
+      auxiliar = num1;
+      num1 = num2;
+      num2 = num1 + auxiliar;
+
+      console.log(num2);
+
+
+   }
+}
+
